a8bbffb6b775c41f7c44058024fb05a393777261
Kwanele <112133974+Kwanele03@users.noreply.github.com>
2025-01-19 02:53:24 +0200

    Update OrderItem.cs

--- Login_SignUp/Models/OrderItem.cs
+++ Login_SignUp/Models/OrderItem.cs
@@ -1,23 +1,8 @@
-﻿
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Login_SignUp.Models
 {
     public class OrderItem
     {
-        [Key]
-        public int Id { get; set; }
 
-        public int Amount { get; set; }
-        public double Price { get; set; }
-
-        public int ProductId { get; set; }
-        [ForeignKey("Id")]
-        public Product Product { get; set; }
-
-        public int OrderId { get; set; }
-        [ForeignKey("OrderId")]
-        public Order Order { get; set; }
     }
 }
