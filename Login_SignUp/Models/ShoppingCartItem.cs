7d784bd08a199b6afb30c37ea4635dab9d64c170
Kwanele <112133974+Kwanele03@users.noreply.github.com>
2025-01-19 02:52:46 +0200

    Update ShoppingCartItem.cs

--- Login_SignUp/Models/ShoppingCartItem.cs
+++ Login_SignUp/Models/ShoppingCartItem.cs
@@ -1,18 +1,9 @@
-﻿
-using System.ComponentModel.DataAnnotations;
 
 namespace Login_SignUp.Models
 {
     public class ShoppingCartItem
     {
-        [Key]
-        public int Id { get; set; }
 
-        public Product Product { get; set; }
-        public int Amount { get; set; }
-
-
-        public string ShoppingCartId { get; set; }
 
     }
 }
